edc4b49684134624239a9b3148266c9cc7bf5da4
Daniel <dammkami@gmail.com>
2019-05-16 22:00:16 -0300

    Added chess icons and change buttons color when clicked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css'; 
-import { isLogicalExpression } from '@babel/types';
+const images = require.context('./icons', true);
 
 class App extends React.Component {
   constructor() {
@@ -21,23 +20,35 @@ class App extends React.Component {
 class Board extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {NUMBER_ROWS: 9, NUMBER_COLUMNS: 9}
+    this.state = {NUMBER_ROWS: 8, NUMBER_COLUMNS: 8}
   }
-
+  
+  render_pieces (pieces, color)
+  {
+    return(pieces.map(p => { return( <Square piece={ p } color = { color } /> )}))
+  }
+  
   render_squares (NUMBER_ROWS, NUMBER_COLUMNS) {
-    let r = {}
-    const limit_low_row = NUMBER_ROWS*2
-    const limit_high_row = (NUMBER_ROWS-2)*NUMBER_COLUMNS
-    for(let i = 0; i < NUMBER_ROWS*NUMBER_COLUMNS; i++) {
-        r[i] = 0
-        if((i+1)%NUMBER_COLUMNS === 0) r[i] = 2
-        i < limit_low_row  || i >= limit_high_row ? r[i] += 1 : r[i] += 0
-    }
+    let rows = {}
+    rows[0] = [`rook`, `knight`, `bishop`, `queen`, `king`, `bishop`, `knight`, `rook`]
+    rows[1] = ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+    rows[7] = ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+    rows[8] = [`rook`, `knight`, `bishop`, `queen`, `king`, `bishop`, `knight`, `rook`]
+    rows[2] = ['', '', '', '', '', '', '', '']
+    rows[3] = ['', '', '', '', '', '', '', '']
+    rows[5] = ['', '', '', '', '', '', '', '']
+    rows[4] = ['', '', '', '', '', '', '', '']
+    rows[6] = ['', '', '', '', '', '', '', '']
 
-    const keys = Object.keys(r)
-    return(keys.map(v => {
-      return r[v] === 3 ? <div className = 'board-row'> <Square key = {v} value= { <img src={logo}/> } />  </div>: r[v] === 2 ? <div className = 'board-row'> <Square key = {v} />  </div> : r[v] === 1 ? <Square key = {v} value= { <img src={logo}/> } /> : <Square/>
-    }))
+    const keys = Object.keys(rows)
+    return(keys.map(key => {
+      return(
+        <div className = 'board-row'>
+        {
+          key <= 1 ? this.render_pieces(rows[key], 'white') : key >= 7 ? this.render_pieces(rows[key], 'black') : this.render_pieces(rows[key], '')
+        }
+      </div>
+      )}))
   }
 
   render () {
@@ -50,13 +61,33 @@ class Board extends React.Component {
 class Square extends React.Component {
   constructor (props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {clicked: false}
   }
   
+  handleClick (e) {
+    var c = this.state.clicked
+
+    if(c) e.target.style.backgroundColor = 'white'
+    else e.target.style.backgroundColor = '#adb6c6'
+
+    this.state = {clicked: !c}
+  }
+
+  render_img () {
+    if(!this.props.color) return 
+    const icon = images(`./${this.props.color}-${this.props.piece}.png`)
+
+    return <img className = 'piece' src={icon} />
+  }
+
   render () {
     return (
       <div>
-        <button className = 'square'>
-          {this.props.value}
+        <button className = 'square' onClick={this.handleClick} img>
+          {
+            this.render_img()
+          }
         </button>
       </div>
     )
